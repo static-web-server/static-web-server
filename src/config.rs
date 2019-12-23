1d2c1b3d3a12ed729a9d757f9a45298130d2ff98
Jose Quintana <joseluisquintana20@gmail.com>
2019-12-24 00:55:55 +0100

    feat: add error page options

--- src/config.rs
+++ src/config.rs
@@ -17,4 +17,18 @@ pub struct Options {
     #[structopt(long, default_value = "./assets", env = "SERVER_ASSETS")]
     /// Assets directory path for add cache headers functionality
     pub assets: String,
+    #[structopt(
+        long,
+        default_value = "./public/50x.html",
+        env = "SERVER_ERROR_PAGE_50X"
+    )]
+    /// HTML file content for 50x errors
+    pub page50x: String,
+    #[structopt(
+        long,
+        default_value = "./public/404.html",
+        env = "SERVER_ERROR_PAGE_404"
+    )]
+    /// HTML file content for 404 errors
+    pub page404: String,
 }
