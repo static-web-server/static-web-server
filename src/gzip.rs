ed94c4ba0f0fdf2859e44fc3f84bbd8686217d55
Jose Quintana <joseluisquintana20@gmail.com>
2020-05-04 01:10:51 +0200

    feat: enable gzip compression only for known text-based file types
    resolves #19

--- src/gzip.rs
+++ src/gzip.rs
@@ -1,10 +1,30 @@
 use flate2::write::GzEncoder;
 use flate2::Compression;
-use iron::headers::{AcceptEncoding, ContentEncoding, Encoding};
+use iron::headers::{AcceptEncoding, ContentEncoding, ContentType, Encoding};
+use iron::mime;
 use iron::prelude::*;
 use iron::AfterMiddleware;
 
 pub struct GzipMiddleware;
+
+const GZIP_TYPES: [&str; 16] = [
+    "text/html",
+    "text/css",
+    "text/javascript",
+    "text/xml",
+    "text/plain",
+    "text/x-component",
+    "application/javascript",
+    "application/x-javascript",
+    "application/json",
+    "application/xml",
+    "application/rss+xml",
+    "application/atom+xml",
+    "font/truetype",
+    "font/opentype",
+    "application/vnd.ms-fontobject",
+    "image/svg+xml",
+];
 
 impl AfterMiddleware for GzipMiddleware {
     fn after(&self, req: &mut Request, mut resp: Response) -> IronResult<Response> {
@@ -13,11 +33,28 @@ impl AfterMiddleware for GzipMiddleware {
             return Ok(resp);
         }
 
+        // Enable Gzip compression only for known text-based file types
+        let enable_gzip = match resp.headers.get::<ContentType>() {
+            Some(content_type) => {
+                let mut v = false;
+                for e in &GZIP_TYPES {
+                    if content_type.0 == e.parse::<mime::Mime>().unwrap() {
+                        v = true;
+                        break;
+                    }
+                }
+
+                v
+            }
+            None => false,
+        };
+
         let accept_gz = match req.headers.get::<AcceptEncoding>() {
             Some(accept) => accept.0.iter().any(|qi| qi.item == Encoding::Gzip),
             None => false,
         };
-        if accept_gz {
+
+        if enable_gzip && accept_gz {
             let compressed_bytes = resp.body.as_mut().map(|b| {
                 let mut encoder = GzEncoder::new(Vec::new(), Compression::fast());
                 {
