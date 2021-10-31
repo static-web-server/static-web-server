6f10ef1aeccc115027a6b599fd3aafec050d6c0c
Jose Quintana <joseluisquintana20@gmail.com>
2021-10-31 12:25:11 +0100

    refactor: disable tracing ansi on windows
    it also optimizes tracing/tracing-subscribe dependencies

--- src/logger.rs
+++ src/logger.rs
@@ -7,10 +7,17 @@ use crate::Result;
 /// Initialize logging builder with its levels.
 pub fn init(level: &str) -> Result {
     let level = level.parse::<Level>()?;
+
+    #[cfg(not(windows))]
+    let enable_ansi = true;
+    #[cfg(windows)]
+    let enable_ansi = false;
+
     match tracing_subscriber::fmt()
         .with_writer(io::stderr)
         .with_max_level(level)
         .with_span_events(FmtSpan::CLOSE)
+        .with_ansi(enable_ansi)
         .try_init()
     {
         Err(err) => Err(anyhow!(err)),
