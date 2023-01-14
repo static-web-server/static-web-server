05b2a923685d0a6348e6bf49d10307fbb2dd16cb
Johnathan Sharratt <johnathan.sharratt@gmail.com>
2023-01-14 23:37:55 +1100

    Now copying the correct wasm binary

--- release.sh
+++ release.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 cargo wasix build --release
-cp -f target/wasm32-wasmer-wasi/release/static-web-server.wasm \
+cp -f target/wasm32-wasmer-wasi/release/static-web-server.rustc.wasm \
   /prog/deploy/wasmer-web/wapm/web-server.wasm
-cp -f target/wasm32-wasmer-wasi/release/static-web-server.wasm \
+cp -f target/wasm32-wasmer-wasi/release/static-web-server.rustc.wasm \
   /prog/packages/static-web-server/web-server.wasm
-cp -f target/wasm32-wasmer-wasi/release/static-web-server.wasm \
+cp -f target/wasm32-wasmer-wasi/release/static-web-server.rustc.wasm \
   /prog/quantochat/wapm/quanto-chat-web/web-server.wasm
