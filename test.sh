7bf573b5ff0bf40c293f62bb6306447bc7d38fab
Johnathan Sharratt <johnathan.sharratt@gmail.com>
2023-02-22 13:48:58 +1100

    Added a networking to the test script

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ cargo wasix build
 PWD=$(pwd)
 cd /prog/wasmer/lib/cli
 cargo run --features compiler,cranelift,debug \
-  -- run --mapdir /public:/prog/deploy/wasmer-web/wapm/public /prog/static-web-server/target/wasm32-wasmer-wasi/debug/static-web-server.rustc.wasm \
+  -- run -d --net --mapdir /public:/prog/deploy/wasmer-web/wapm/public /prog/static-web-server/target/wasm32-wasmer-wasi/debug/static-web-server.rustc.wasm \
   -- -p 9080 --log-level trace
 cd $PWD
 
