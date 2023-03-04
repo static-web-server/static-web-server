ccbad3e2d51505a2ff939e77e8d441b67bcf5ee7
Johnathan Sharratt <johnathan.sharratt@gmail.com>
2023-03-04 17:01:23 +1100

    Switched to different binary that's smaller

--- test-release.sh
+++ test-release.sh
@@ -4,6 +4,6 @@ cargo wasix build --no-default-features --release
 PWD=$(pwd)
 cd /prog/wasmer/lib/cli
 cargo run --release --features compiler,cranelift \
-  -- run --net --mapdir /public:/prog/deploy/wasmer-web/wapm/public /prog/static-web-server/target/wasm32-wasmer-wasi/release/static-web-server.rustc.wasm \
-  -- -p 9080 --log-level info
+  -- run --net --mapdir /public:/prog/deploy/wasmer-web/wapm/public /prog/static-web-server/target/wasm32-wasmer-wasi/release/static-web-server.wasm \
+  -- -p 9080 --log-level warn
 cd $PWD
