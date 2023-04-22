ff49cdaa65d727baa561d6a00ba8547cd4c6dc47
Johnathan Sharratt <johnathan.sharratt@gmail.com>
2023-04-22 15:48:09 +1000

    Modified the scripts for release

--- test-release.sh
+++ test-release.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
 cargo wasix build --release --no-default-features
 
-PWD=$(pwd)
+CUR=$(pwd)
 cd /prog/wasmer/lib/cli
 cargo run --release --features compiler,cranelift \
-  -- run --net --mapdir /public:/prog/deploy/wasmer-web/wapm/public /prog/static-web-server/target/wasm32-wasmer-wasi/release/static-web-server.wasm \
-  -- -p 9080 --log-level warn
-cd $PWD
+  -- run --net --mapdir /public:$CUR/root/public --mapdir /cfg:$CUR/root/cfg $CUR/target/wasm32-wasmer-wasi/release/static-web-server.wasm \
+  -- -p 9080 --log-level info
+cd $CUR
