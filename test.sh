fcc5f561311460cf22b2aa4fcc16ab9ead32adba
Johnathan Sharratt <johnathan.sharratt@gmail.com>
2023-05-19 17:02:28 +1000

    Fixes for the test script

--- test.sh
+++ test.sh
@@ -4,6 +4,6 @@ cargo wasix build --no-default-features
 CUR=$(pwd)
 cd /prog/wasmer/lib/cli
 cargo run --features compiler,cranelift,debug \
-  -- run --net --mapdir /public:$CUR/root/public --mapdir /cfg:$CUR/root/cfg $CUR/target/wasm32-wasmer-wasi/debug/static-web-server.rustc.wasm \
+  -- run --enable-async-threads --net --mapdir /public:$CUR/root/public --mapdir /cfg:$CUR/root/cfg $CUR/target/wasm32-wasmer-wasi/debug/static-web-server.rustc.wasm \
   -- --log-level info
 cd $CUR
