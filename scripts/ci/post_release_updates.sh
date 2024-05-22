efbaf6fddd54b60f2985cb2b0ce43feb440db9b8
Jose Quintana <joseluisquintana20@gmail.com>
2024-05-23 00:11:32 +0200

    refactor: improve `download-and-install.md` doc file

--- scripts/ci/post_release_updates.sh
+++ scripts/ci/post_release_updates.sh
@@ -39,13 +39,17 @@ echo "Calculating and verifying checksum file..."
 sha256sum static-web-server-* > $checksum_file_name
 sha256sum -c $checksum_file_name
 
-echo "Uploading checksum file to $SERVER_VERSION GitHub release..."
-release_id=$(cat $release_json | jq -r ".id")
-curl -LX POST \
-    --data-binary @$checksum_file_name \
-    --header "Authorization: token $GITHUB_TOKEN" \
-    --header "Content-Type: application/octet-stream" \
-    https://uploads.github.com/repos/static-web-server/static-web-server/releases/$release_id/assets?name=$checksum_file_name
+upload_checksum=${UPLOAD_CHECKSUM:-""}
+
+if [[ -n "$upload_checksum" ]] && [[ "$upload_checksum" = "true" ]]; then
+    echo "Uploading checksum file to $SERVER_VERSION GitHub release..."
+    release_id=$(cat $release_json | jq -r ".id")
+    curl -LX POST \
+        --data-binary @$checksum_file_name \
+        --header "Authorization: token $GITHUB_TOKEN" \
+        --header "Content-Type: application/octet-stream" \
+        https://uploads.github.com/repos/static-web-server/static-web-server/releases/$release_id/assets?name=$checksum_file_name
+fi
 
 echo "Checksum file uploaded successfully!"
 echo
@@ -57,13 +61,12 @@ release_date=$(date +%Y-%m-%d)
 filename_version="static-web-server-$SERVER_VERSION"
 filename_version_num="static-web-server-$server_version_num"
 
-sed_fname=""
-if [[ $(uname -s) = "Darwin" ]]; then
-    sed_fname=" ''"
-fi
+page_download_install_generated=docs/content/download-and-install.generated.md
+sed_bk=".bk"
 
 # Replace placeholder occurrences
-sed "s/{{RELEASE_DATE}}/$release_date/g" docs/content/download-and-install.tmpl.md > docs/content/download-and-install.md
+echo "<!-- Content generated. DO NOT EDIT. -->" > $page_download_install_generated
+sed "s/{{RELEASE_DATE}}/$release_date/g" docs/content/download-and-install.md >> $page_download_install_generated
 
 while read -r line; do
     checksum=$(echo $line | awk -F ' ' '{print $1}')
@@ -71,24 +74,26 @@ while read -r line; do
     placeholder_checksum=$(echo $filename | sed "s/^$filename_version-//")
 
     if [[ "$placeholder_checksum" = "$filename_version_num.tar.gz" ]]; then
-        sed -i$sed_fname -e "s/{{SRC_TAR}}/$checksum/" docs/content/download-and-install.md
+        sed -i$sed_bk -e "s/{{SRC_TAR}}/$checksum/" $page_download_install_generated
         continue
     fi
 
     if [[ "$placeholder_checksum" = "$filename_version_num.zip" ]]; then
-        sed -i$sed_fname -e "s/{{SRC_ZIP}}/$checksum/" docs/content/download-and-install.md
+        sed -i$sed_bk -e "s/{{SRC_ZIP}}/$checksum/" $page_download_install_generated
         continue
     fi
 
-    sed -i$sed_fname -e "s/{{$placeholder_checksum}}/$checksum/" docs/content/download-and-install.md
+    sed -i$sed_bk -e "s/{{$placeholder_checksum}}/$checksum/" $page_download_install_generated
 done < <(cat "$release_dir/$checksum_file_name")
 
-sed -i$sed_fname -e "s/{{RELEASE_VERSION}}/$SERVER_VERSION/g" docs/content/download-and-install.md
-sed -i$sed_fname -e "s/{{RELEASE_VERSION_NUM}}/$server_version_num/g" docs/content/download-and-install.md
-echo "Download and install page updated!"
+sed -i$sed_bk -e "s/{{RELEASE_VERSION}}/$SERVER_VERSION/g" $page_download_install_generated
+sed -i$sed_bk -e "s/{{RELEASE_VERSION_NUM}}/$server_version_num/g" $page_download_install_generated
+rm -rf docs/content/*.bk
+echo "Download and install page generated!"
 
 # Update current installer script version
-sed -i$sed_fname -e "s/version=\".*\"/version=\"$server_version_num\"/g" $cwd/scripts/installer.sh
+sed -i$sed_bk -e "s/version=\".*\"/version=\"$server_version_num\"/g" $cwd/scripts/installer.sh
+rm -rf $cwd/scripts/*.bk
 echo "Installer script $server_version_num version updated!"
 
 echo
