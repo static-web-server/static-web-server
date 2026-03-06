f23d582b0275bc1a8da5ae1252f4f96ba860c2a5
Jose Quintana <joseluisquintana20@gmail.com>
2026-03-06 22:38:40 +0100

    fix: disable symlinks option does not work properly

--- tests/compression_static.rs
+++ tests/compression_static.rs
@@ -28,7 +28,7 @@ mod tests {
 
     #[tokio::test]
     async fn compression_static_file_exists() {
-        let archive_path = PathBuf::from("tests/fixtures/public/index.html.gz");
+        let archive_path = PathBuf::from("tests/fixtures/public/index.htm.br");
         let archive_buf =
             std::fs::read(&archive_path).expect("unexpected error when reading archive file");
         let archive_buf = Bytes::from(archive_buf);
@@ -36,6 +36,7 @@ mod tests {
         let opts = fixture_settings("toml/handler_fixtures.toml");
         let general = General {
             compression_static: true,
+            index_files: "index.htm, index.html".to_owned(),
             ..opts.general
         };
         let req_handler_opts = fixture_req_handler_opts(general, opts.advanced);
@@ -43,7 +44,7 @@ mod tests {
 
         let mut req = Request::default();
         *req.method_mut() = hyper::Method::GET;
-        *req.uri_mut() = "http://localhost/index.html".parse().unwrap();
+        *req.uri_mut() = "http://localhost/index.htm".parse().unwrap();
         req.headers_mut().insert(
             http::header::ACCEPT_ENCODING,
             "gzip, deflate, br".parse().unwrap(),
@@ -56,7 +57,7 @@ mod tests {
 
                 assert_eq!(res.status(), 200);
                 assert!(!headers.contains_key("content-length"));
-                assert_eq!(headers["content-encoding"], "gzip");
+                assert_eq!(headers["content-encoding"], "br");
                 assert_eq!(headers["accept-ranges"], "bytes");
                 assert!(!headers["last-modified"].is_empty());
                 assert_eq!(
@@ -153,9 +154,8 @@ mod tests {
                 let headers = res.headers();
 
                 assert_eq!(res.status(), 200);
-                assert!(headers.contains_key("content-length"));
                 assert_eq!(headers["accept-ranges"], "bytes");
-                assert!(!headers.contains_key("content-encoding"));
+                assert_eq!(headers["content-encoding"], "br");
                 assert!(!headers["last-modified"].is_empty());
                 assert_eq!(
                     &headers["content-type"], "text/html",
@@ -178,6 +178,7 @@ mod tests {
             directory_listing_format: DirListFmt::Html,
             ignore_hidden_files: false,
             disable_symlinks: false,
+            index_files: "index.htm, index.html".to_owned(),
             ..opts.general
         };
         let req_handler_opts = fixture_req_handler_opts(general, opts.advanced);
@@ -187,7 +188,7 @@ mod tests {
         *req.method_mut() = hyper::Method::GET;
         *req.uri_mut() = "http://localhost".parse().unwrap();
         req.headers_mut()
-            .insert(http::header::ACCEPT_ENCODING, "zstd, gzip".parse().unwrap());
+            .insert(http::header::ACCEPT_ENCODING, "br, gzip".parse().unwrap());
 
         let remote_addr = Some(REMOTE_ADDR.parse::<SocketAddr>().unwrap());
         match req_handler.handle(&mut req, remote_addr).await {
@@ -196,7 +197,7 @@ mod tests {
 
                 assert_eq!(res.status(), 200);
                 assert_eq!(headers["accept-ranges"], "bytes");
-                assert_eq!(headers["content-encoding"], "gzip");
+                assert_eq!(headers["content-encoding"], "br");
                 assert!(!headers["last-modified"].is_empty());
                 assert_eq!(
                     &headers["content-type"], "text/html",
@@ -210,7 +211,7 @@ mod tests {
 
     #[tokio::test]
     async fn compression_static_zstd_file_exists() {
-        let archive_path = PathBuf::from("tests/fixtures/public/main.js.zst");
+        let archive_path = PathBuf::from("tests/fixtures/public/assets/main.css.zst");
         let archive_buf =
             std::fs::read(&archive_path).expect("unexpected error when reading archive file");
         let archive_buf = Bytes::from(archive_buf);
@@ -226,7 +227,7 @@ mod tests {
 
         let mut req = Request::default();
         *req.method_mut() = hyper::Method::GET;
-        *req.uri_mut() = "http://localhost/main.js".parse().unwrap();
+        *req.uri_mut() = "http://localhost/assets/main.css".parse().unwrap();
         req.headers_mut().insert(
             http::header::ACCEPT_ENCODING,
             "gzip, deflate, br, zstd".parse().unwrap(),
@@ -243,8 +244,8 @@ mod tests {
                 assert_eq!(headers["accept-ranges"], "bytes");
                 assert!(!headers["last-modified"].is_empty());
                 assert_eq!(
-                    &headers["content-type"], "text/javascript",
-                    "content-type is not javascript"
+                    &headers["content-type"], "text/css",
+                    "content-type is not css"
                 );
                 assert_eq!(headers["vary"], "accept-encoding");
 
